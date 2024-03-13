826def643bb52b6910897ff7ac552b5b5dd8c179
syk531 <syk531@pluxity.com>
2024-03-13 09:48:24 +0900

    init

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,18 @@
+package hello.advanced.trace;
+
+
+import java.util.UUID;
+
+public class TraceId {
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString();
+    }
+}
